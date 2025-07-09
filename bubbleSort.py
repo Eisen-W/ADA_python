5f79376f667fad604fdf561645e67aa66a00e024
Eisen-W <minccinotrainer03@gmail.com>
2025-07-09 21:13:17 +0530

    bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,3 +1,21 @@
 def bubbleSort(arr, size):
-    for i in range(size -1, 0, -1):
-                
+    for n in range(size -1, 0, -1):
+        swapped = False
+
+        for i in range(n):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+        
+        if not swapped:
+            break
+
+arr = [34, 56, 12, 76, 21]
+size = len(arr)
+print("sorted array:")
+print(arr)
+
+bubbleSort(arr, size)
+
+print("sorted array")
+print(arr)
